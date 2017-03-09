e1930122833d655bf6bc0fc76e90ee3a524c8c64
Kevin Gin <kevinagin@gmail.com>
2017-03-09 14:39:17 -0800

    (feat) grabs real answerKey data

--- app/Keys.js
+++ app/Keys.js
@@ -19,40 +19,77 @@ export default class Keys extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      courseData: {
-        classId: 2,
-        className: 'Badminton 101',
-        answerKeys: [{
-          keyId: 1,
-          keyName: 'Midterm',
-          answers: 'answerstring'
-        },{
-          keyId: 2,
-          keyName: 'Final Exam',
-          answers: 'answerstring2'
-        },{
-          keyId: 1,
-          keyName: 'Precourse',
-          answers: 'answerstring3'
-        },{
-          keyId: 2,
-          keyName: 'Quiz 1',
-          answers: 'answerstring4'
-        }]
-      }
+      courseData: {}
     }
   }
+      // Example courseData, as passed back from Server.
+      // courseData: {
+      //   classId: 2,
+      //   classname: 'Badminton 101',
+      //   answerkey: [{
+      //     keyId: 1,
+      //     keyName: 'Midterm',
+      //     answers: 'answerstring'
+      //   },{
+      //     keyId: 2,
+      //     keyName: 'Final Exam',
+      //     answers: 'answerstring2'
+      //   }]
+      // }
 
   componentDidMount() {
-    for (key in this.props) {
-      console.log(key);
-    }
+    console.log('component mounted')
+    // var props = this.props;
+    // var courseId = this.props.course.ClassId;
+    
+
+    // // for (key in this.props) {
+    // //   console.log(key);
+    // // }
+
+    // console.log(courseId)
+    this.getKeys();
+  }
+
+
+// http://localhost:8080/api/getKeysForClass?class_Id=8&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiJzYW1wbGUiLCJ1c2VydHlwZSI6InRlYWNoZXIiLCJ1c2VySWQiOjIsImlhdCI6MTQ4OTA5NzU5MSwiZXhwIjoxNDg5MTMzNTkxfQ.LEEoi49DULgtk0f1_BHh_jsOCo_evmNMcuwBqgMzQaU
+
+  getKeys() {
+    var context = this;
+    var classId = this.props.course.ClassId;
+
+    console.log('getKeysCalled')
+    AsyncStorage.getItem('@teachersPetToken', (err, token) => {
+      var config = {
+        method: 'get',
+        url: 'http://10.7.24.223:8080/api/getKeysForClass?token=' + token +'&class_Id=' + classId
+      }
+      axios(config)
+        .then((data) => {
+          console.log('getKeys success 2')
+          // console.log(data.request._response);
+          var courseData = JSON.parse(data.request._response)
+          
+          console.log('courseData did not throw error 2')
+          console.log(courseData);
+          context.setState({
+            courseData: courseData
+          })
+
+
+          console.log('set State successfully')
+        })
+        .catch((err) => {
+          // ERROR HANDLING
+          console.log('ERROR - catch called in Courses.js')
+        })
+    })
   }
 
   render() {
     var user = this.props.user;
     var course = this.props.course;
-    var keys = this.state.courseData.answerKeys;
+    var keys = this.state.courseData.answerkey || [];
 
     var keyEntries = keys.map(key => 
       <KeyEntry course={key}/>
